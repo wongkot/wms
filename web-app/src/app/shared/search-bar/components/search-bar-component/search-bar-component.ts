89b7ea792fdff24a0642ef46bbca1e186c3adba5
Wongkot <wongkot.v@gmail.com>
2025-08-16 20:27:08 +0700

    Add search bar module

---
+++ web-app/src/app/shared/search-bar/components/search-bar-component/search-bar-component.ts
@@ -0,0 +1,23 @@
+import { Component, output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: false,
+  templateUrl: './search-bar-component.html',
+  styleUrl: './search-bar-component.css'
+})
+export class SearchBarComponent {
+  textChanged = output<string>();
+	public searchForm: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+		this.searchForm = this.fb.group({
+			searchTerm: '',
+		});
+	}
+
+	public onSubmit(): void {
+		this.textChanged.emit(this.searchForm.get('searchTerm')?.value);
+	}
+}
