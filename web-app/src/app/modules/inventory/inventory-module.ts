6a63cfdd99617ebc86a3b25967a4f94c309c09d1
Wongkot <wongkot.v@gmail.com>
2025-08-31 17:28:22 +0700

    Implement inventory management page

--- web-app/src/app/modules/inventory/inventory-module.ts
+++ web-app/src/app/modules/inventory/inventory-module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InventoryManagementPage } from '@app/modules/inventory/pages/inventory-management-page/inventory-management-page';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from '@app/shared/button/button-module';
+import { DropdownModule } from '@app/shared/dropdown/dropdown-module';
+import { PaginationModule } from '@app/shared/pagination/pagination-module';
+import { SearchBarModule } from '@app/shared/search-bar/search-bar-module';
+import { TableModule } from '@app/shared/table/table-module';
 
 const ROUTES: Routes = [
 	{ path: '', pathMatch: 'full', component: InventoryManagementPage },
@@ -13,6 +18,11 @@ const ROUTES: Routes = [
   ],
   imports: [
     CommonModule,
+    TableModule,
+    SearchBarModule,
+    PaginationModule,
+    DropdownModule,
+    ButtonModule,
     RouterModule.forChild(ROUTES),
   ]
 })
---
+++ web-app/src/app/modules/inventory/services/state/inventory-management-state-service.ts
@@ -0,0 +1,79 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Pagination } from '@app/core/models/pagination';
+import { InventoryProduct } from '@app/modules/inventory/models/inventory-product';
+import { InventoryService } from '@app/modules/inventory/services/data/inventory-service';
+import { MockInventoryService } from '@app/modules/inventory/services/data/mock-inventory-service';
+import { TableSortState } from '@app/shared/table/models/table-sort-state';
+import { finalize } from 'rxjs';
+
+@Injectable()
+export class InventoryManagementStateService {
+  private _isLoading = signal<boolean>(false);
+  private _errorMessage = signal<string>('');
+  private _selectedPageSize = signal<number>(10);
+  private _selectedPage = signal<number>(1);
+  private _searchTerm = signal<string>('');
+  private _selectedCategory = signal<string>('');
+  private _currentSortState = signal<TableSortState>({ columnProp: '', isAsc: true });
+  private _displayInventoryProducts = signal<Pagination<InventoryProduct>>({
+    currentPage: this._selectedPage(),
+    pageSize: this._selectedPageSize(),
+    totalItems: 0,
+    items: [],
+  });
+  private _inventoryService: InventoryService = inject(MockInventoryService);
+
+  constructor() {
+    this.loadInventoryProducts()
+  }
+
+  public get displayInventoryProducts() {
+    return this._displayInventoryProducts.asReadonly();
+  }
+
+  public get selectedPage() {
+    return this._selectedPage.asReadonly();
+  }
+
+  public get selectedPageSize() {
+    return this._selectedPageSize.asReadonly();
+  }
+
+  public get searchTerm() {
+    return this._searchTerm.asReadonly();
+  }
+
+  public get selectedCategory() {
+    return this._selectedCategory.asReadonly();
+  }
+
+  public get currentSortState() {
+    return this._currentSortState.asReadonly();
+  }
+
+  loadInventoryProducts(options?: { page?: number, pageSize?: number, query?: string, category?: string, sort?: TableSortState }): void {
+    this._isLoading.set(true);
+    this._errorMessage.set('');
+
+    let page = options?.page != undefined ? options.page : this._selectedPage();
+    let pageSize = options?.pageSize != undefined ? options.pageSize : this._selectedPageSize();
+    let query = options?.query != undefined ? options.query : this._searchTerm();
+    let category = options?.category != undefined ? options.category : this._selectedCategory();
+    let sort = options?.sort != undefined ? options.sort : this._currentSortState();
+
+    this._inventoryService.getInventoryProducts(page, pageSize, query, category, `${sort?.columnProp}:${sort?.isAsc ? 'asc' : 'desc'}`).pipe(
+      finalize(() => { this._isLoading.set(false); }),
+    ).subscribe({
+      next: (pageInventoryProducts) => {
+        this._displayInventoryProducts.set(pageInventoryProducts);
+        this._isLoading.set(false);
+
+        this._currentSortState.set(sort);
+        if (this._searchTerm() != query) this._searchTerm.set(query);
+        if (this._selectedCategory() != category) this._selectedCategory.set(category);
+        if (this._selectedPage() != page) this._selectedPage.set(page);
+        if (this._selectedPageSize() != pageSize) this._selectedPageSize.set(pageSize);
+      }
+    });
+  }
+}
