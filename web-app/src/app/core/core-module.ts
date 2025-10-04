10134968fede216174c9b8e48cf81d54b74eea52
Wongkot <wongkot.v@gmail.com>
2025-10-05 00:37:55 +0700

    Add notification button to navbar

--- web-app/src/app/core/core-module.ts
+++ web-app/src/app/core/core-module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavBar } from '@app/core/components/nav-bar/nav-bar';
 import { RouterModule } from '@angular/router';
 import { ThemeButtonComponent } from '@app/core/components/theme-button-component/theme-button-component';
+import { NotificationButtonComponent } from '@app/core/components/notification-button-component/notification-button-component';
+import { TimeAgoPipe } from './pipes/time-ago-pipe';
 
 @NgModule({
   declarations: [
     NavBar,
-    ThemeButtonComponent
+    ThemeButtonComponent,
+    NotificationButtonComponent,
+    TimeAgoPipe
   ],
   imports: [
     CommonModule,
